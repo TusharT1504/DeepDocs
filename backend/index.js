d4b3b6a446db1e508f3d058f8547ae122cc26d78
TusharT1504 <tushart1504@gmail.com>
2025-06-17 22:20:14 +0530

    chore: cors updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,18 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+
+        origin:[
+            "https://deep-docs-five.vercel.app",
+            "https://deep-docs-tushars-projects-4bd6811b.vercel.app",
+            "https://deep-docs-git-main-tushars-projects-4bd6811b.vercel.app",
+            "http://localhost:5000"
+    ],
+        credentials:true,
+    })
+);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 

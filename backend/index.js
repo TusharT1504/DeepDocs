1409ca400f87c305a8c5e64626681f06cdbb3bcf
TusharT1504 <tushart1504@gmail.com>
2025-06-17 21:57:32 +0530

    refactor:  dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const dotenv=require("dotenv");
+dotenv.config();
 
 const app = express();
 
